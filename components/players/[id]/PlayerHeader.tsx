af1450b5ac3c0d950bbaba87ad4420a08abcddc1
adamxu29 <adam.xu290@gmail.com>
2026-08-29 19:10:51 -0600

    start player profile structure

---
+++ components/players/[id]/PlayerHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PlayerNav from './PlayerNav'
+
+function PlayerHeader() {
+  return (
+    <div>
+        {/* Name, photo, team, basic/career stats? */}
+        <PlayerNav/>
+    </div>
+  )
+}
+
+export default PlayerHeader
---
+++ components/players/[id]/PlayerProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlayerProfile({ id }: { id: number }) {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default PlayerProfile
